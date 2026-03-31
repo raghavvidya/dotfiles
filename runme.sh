5708d5633d853b31130ca9b845c63863cba1ab42
Raghavendra Kamath <raghavvidya@gmail.com>
2026-03-31 21:41:13 +0100

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -3,3 +3,6 @@
 
 # Ensure install.sh runs only in zsh interactive shells
 echo '. /home/vscode/dotfiles/install.sh' >> /home/vscode/.zshrc
+mkdir -p /home/vscode/.kube/
+curl -fsSL https://raw.githubusercontent.com/ministryofjustice/analytical-platform/e44cf29bfe0400af600987e1bc4763cdff83099a/.devcontainer/src/kubernetes/config -o /home/vscode/.kube/config
+
