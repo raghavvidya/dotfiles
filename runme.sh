f8fb23064f13fe380e156cf08c2736a53c08229c
Raghavendra Kamath <raghavvidya@gmail.com>
2026-03-31 21:18:09 +0100

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,1 +1,5 @@
-. /home/vscode/dotfiles/install.sh
+#!/bin/sh
+# POSIX-safe bootstrap
+
+# Ensure install.sh runs only in zsh interactive shells
+echo '. /home/vscode/dotfiles/install.sh' >> /home/vscode/.zshrc
